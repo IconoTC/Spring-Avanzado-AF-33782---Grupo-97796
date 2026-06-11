c90042d1cfdea1a55f7983b80cec5c935295f720
Javier <35864946+jmagit@users.noreply.github.com>
2026-06-11 11:44:34 +0200

    repositorio crud y consultas

--- demo-data/src/main/java/com/example/DemoDataApplication.java
+++ demo-data/src/main/java/com/example/DemoDataApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Sort;
+
+import com.example.contracts.domain.repositories.ActoresRepository;
+import com.example.domain.entities.Actor;
 
 @SpringBootApplication
 public class DemoDataApplication {
@@ -12,9 +16,38 @@ public class DemoDataApplication {
 		SpringApplication.run(DemoDataApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner ejemplos() {
+//	@Bean
+	CommandLineRunner crud(ActoresRepository dao) {
 		return arg -> {
+			var actor = dao.save(new Actor("Pepito", "Grillo"));
+			var id = actor.getActorId();
+			var leido = dao.findById(id);
+			if(leido.isEmpty()) {
+				System.err.println("Actor no encontrado");
+				return;
+			}
+			actor = leido.get();
+			actor.setFirstName(actor.getFirstName().toUpperCase());
+			dao.save(actor);
+			dao.findAll().forEach(System.out::println);
+			dao.deleteById(id);
+			if(!dao.existsById(id))
+				System.err.println("Actor %d no encontrado".formatted(id));
+			dao.deleteById(id);
+		};
+	}
+
+	@Bean
+	CommandLineRunner consultas(ActoresRepository dao) {
+		return arg -> {
+//			dao.findTop5ByFirstNameStartingWithOrderByLastNameDesc("P").forEach(System.out::println);
+//			dao.findTop5ByFirstNameStartingWith("P", Sort.by("FirstName").descending()).forEach(System.out::println);
+			
+//			dao.findByActorIdGreaterThanEqual(195).forEach(System.out::println);
+//			dao.findNovedadesJPQL(195).forEach(System.out::println);
+//			dao.findNovedadesSQL(195).forEach(System.out::println);
+//			dao.findAll((root, query, builder) -> builder.greaterThanOrEqualTo(root.get("actorId"), 195)).forEach(System.out::println);
+			
 		};
 	}
 
---
+++ demo-data/src/main/java/com/example/contracts/domain/repositories/ActoresRepository.java
@@ -0,0 +1,26 @@
+package com.example.contracts.domain.repositories;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Meta;
+import org.springframework.data.jpa.repository.NativeQuery;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.domain.entities.Actor;
+
+public interface ActoresRepository extends JpaRepository<Actor, Integer>, JpaSpecificationExecutor<Actor> {
+	List<Actor> findTop5ByFirstNameStartingWithOrderByLastNameDesc(String prefijo);
+	List<Actor> findTop5ByFirstNameStartingWith(String prefijo, Sort orderBy);
+	
+	@Meta(comment = "Consulta DSL")
+	List<Actor> findByActorIdGreaterThanEqual(int id);
+	@Meta(comment = "Consulta JPQL")
+	@Query("from Actor a where a.actorId >= ?1")
+	List<Actor> findNovedadesJPQL(int id);
+	@Meta(comment = "Consulta SQL")
+	@NativeQuery("select * from actor a where a.actor_id >= :id")
+	List<Actor> findNovedadesSQL(int id);
+}
