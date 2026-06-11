9bf1da7a4906c0cbc5a3fbc0443cd97a281df3f2
Javier <35864946+jmagit@users.noreply.github.com>
2026-06-11 13:05:16 +0200

    @Transactional

--- demo-data/src/main/java/com/example/DemoDataApplication.java
+++ demo-data/src/main/java/com/example/DemoDataApplication.java
@@ -1,17 +1,21 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.contracts.domain.repositories.ActoresRepository;
 import com.example.domain.entities.Actor;
 
 @SpringBootApplication
 public class DemoDataApplication {
-
+	@Autowired @Lazy DemoDataApplication self;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoDataApplication.class, args);
 	}
@@ -47,8 +51,31 @@ public class DemoDataApplication {
 //			dao.findNovedadesJPQL(195).forEach(System.out::println);
 //			dao.findNovedadesSQL(195).forEach(System.out::println);
 //			dao.findAll((root, query, builder) -> builder.greaterThanOrEqualTo(root.get("actorId"), 195)).forEach(System.out::println);
-			
+			dao.findAll((root, query, builder) -> builder.lessThan(root.get("actorId"), 4)).forEach(System.out::println);
+//			self.navegacion(dao);
+			try {
+//				self.modifica(dao);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			dao.findNovedadesJPQL(200).forEach(System.out::println);
 		};
 	}
+	
+	@Transactional
+	void navegacion(ActoresRepository dao) {
+		dao.findNovedadesJPQL(199).forEach(a -> {
+			System.out.println(a.toString());
+			a.getFilmActors().forEach(p -> System.out.println("\t%s".formatted(p.getFilm().getTitle())));
+		});
+	}
 
+	@Transactional(rollbackFor = Exception.class)
+	void modifica(ActoresRepository dao) {
+		dao.save(new Actor("Pepito", "Grillo"));
+		dao.save(new Actor("Carmelo", "Coton"));
+//		dao.deleteById(1);
+	}
+
+	
 }
--- demo-data/src/main/java/com/example/contracts/domain/repositories/ActoresRepository.java
+++ demo-data/src/main/java/com/example/contracts/domain/repositories/ActoresRepository.java
@@ -3,6 +3,7 @@ package com.example.contracts.domain.repositories;
 import java.util.List;
 
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Meta;
@@ -16,6 +17,7 @@ public interface ActoresRepository extends JpaRepository<Actor, Integer>, JpaSpe
 	List<Actor> findTop5ByFirstNameStartingWith(String prefijo, Sort orderBy);
 	
 	@Meta(comment = "Consulta DSL")
+	@EntityGraph(attributePaths = {"filmActors.film"})
 	List<Actor> findByActorIdGreaterThanEqual(int id);
 	@Meta(comment = "Consulta JPQL")
 	@Query("from Actor a where a.actorId >= ?1")
